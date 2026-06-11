d1dc34646d3a9dfe91b0015b8d44faafda53f545
anhthu <anhthuttiktok2026@gmail.com>
2026-06-11 23:30:12 +0700

    Show V12

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,46 @@
-self.addEventListener("install", (event) => {
-  console.log("SW installing");
+const CACHE_NAME = "techfix-v12";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./db.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.PNG"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+
   self.skipWaiting();
+
 });
 
-self.addEventListener("activate", (event) => {
-  console.log("SW activated");
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+
 });
